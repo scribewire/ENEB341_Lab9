c2896ba578904dcd04ff1524c26449ac41674b1d
scriber <77025035+scribewire@users.noreply.github.com>
2024-11-06 11:36:42 -0500

    Changes for lab

--- functions.c
+++ functions.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "functions.h"
 
-#define MAX_SIZE 100  // Maximum matrix size
+#define MAX_SIZE 100  // Maximum matrix
 
 void transposeMatrix(int original[][MAX_SIZE], int transposed[][MAX_SIZE], int row, int col) {
     for (int i = 0; i < row; i++) {
--- functions.h
+++ functions.h
@@ -2,9 +2,8 @@
 #ifndef MATRIX_OPERATIONS_H
 #define MATRIX_OPERATIONS_H
 
-#define MAX_SIZE 100  // Maximum matrix size
+#define MAX_SIZE 100  // Maximum matrix resolution
 
-// Function prototypes
 void transposeMatrix(int original[][MAX_SIZE], int transposed[][MAX_SIZE], int row, int col);
 void printMatrix(int matrix[][MAX_SIZE], int row, int col) ;
 
